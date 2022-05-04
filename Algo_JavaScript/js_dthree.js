dbac89c302f8cbc7d3c62dfc074c6789f29c33b1
Amani Mkamba <amani.mkamba@gmail.com>
2022-05-03 23:23:42 -0400

    js_dthree_v1

---
+++ Algo_JavaScript/js_dthree.js
@@ -0,0 +1,36 @@
+// Swap variables
+
+var firstFruit = "Apples";
+var secondFruit = "Oranges";
+
+var temp = firstFruit;
+firstFruit = secondFruit;
+secondFruit = temp;
+
+console.log(secondFruit + " and " + firstFruit)
+
+
+// while loop
+
+var start = 0;
+var end = 10;
+
+while(start < end) {
+    // console.log("start: " + start + " end: " + end);
+    start += 1;
+    end -= 1;
+}
+
+
+// reversing and array
+
+var arr = ["a", "b"]
+
+for( i = arr.length-1; i < arr.length; i--) {
+    console. log(i)
+}
+
+
+
+
+
