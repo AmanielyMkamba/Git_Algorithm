fe6f68f964b302238edf3ee70e14c0d3352d29fe
Amani Mkamba <amani.mkamba@gmail.com>
2022-05-09 23:07:57 -0400

    js_dfour_v2

--- Algo_JavaScript/js_dfour.js
+++ Algo_JavaScript/js_dfour.js
@@ -10,6 +10,32 @@ console.log(random);
 
 // dice roller
 
-// function diceRoller() {
-//     var roll = Math.random();
-// }
+function diceRoller() {
+    var roll = Math.random();
+}
+
+// consult oracle
+
+var lifesAnswers = [
+    "It is certain.",
+    "It is decidedly so.",
+    "Without a doubt.",
+    "Yes – definitely.",
+    "You may rely on it.",
+    "As I see it, yes.",
+    "Most likely.",
+    "Outlook good.",
+    "Yes.",
+    "Signs point to yes.",
+    "Reply hazy, try again.",
+    "Ask again later.",
+    "Better not tell you now.",
+    "Cannot predict now.",
+    "Concentrate and ask again.",
+    "Don't count on it.",
+    "My reply is no.",
+    "My sources say no.",
+    "Outlook not so good.",
+    "Very doubtful."
+];
+
