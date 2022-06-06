dec895cd49882be06539fea82a4464b5af853dbc
Amani Mkamba <amani.mkamba@gmail.com>
2022-06-06 11:20:55 -0400

    first_d5

---
+++ Algo_JavaScript/js_d5.js
@@ -0,0 +1,10 @@
+var monster = {
+    id: 1,
+    name: "Bulbasaur",
+    types: ["poison", "grass"]
+};
+
+console.log(monster["id"])
+console.log(monster["name"])
+console.log(monster["types"][0])
+console.log(monster["types"][1])
