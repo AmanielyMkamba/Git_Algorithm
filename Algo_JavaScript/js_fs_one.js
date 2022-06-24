0824147aded66ed008d50d20a0dd594fba16f997
Amani Mkamba <amani.mkamba@gmail.com>
2022-06-23 21:05:29 -0400

    Create js_fs_one.js

---
+++ Algo_JavaScript/js_fs_one.js
@@ -0,0 +1,28 @@
+// console.log("Amani")
+// console.log("Amani ".length-1)
+//console.log("Hi Amani" + " How are you?")
+//console.log(null)
+
+// let isSummertime = false;
+
+// if (isSummertime) {
+//     console.log("Go outside")
+// } else {
+//     console.log("Stay inside the house")
+// }
+
+let likesAdventureMovies = false
+let likesHorrorMoves = false
+let likesRomanticComedies = false
+
+if(likesAdventureMovies) {
+    console.log("Go the movies")
+}
+if(likesHorrorMoves) {
+    console.log("Go with friend")
+}
+if(likesRomanticComedies) {
+    console.log("Stay at Home")
+} else {
+    console.log("Go shopping")
+}
