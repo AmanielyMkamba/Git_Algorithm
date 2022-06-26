2b20c705d13c9983478d5b8ef8a0b868b72fa941
Amani Mkamba <amani.mkamba@gmail.com>
2022-06-25 22:14:29 -0400

    Create looops.js

---
+++ Algo_JavaScript/looops.js
@@ -0,0 +1,12 @@
+let count = 3;
+while(count >= 1) {
+    console.log("count is ", count)
+    count --
+}
+
+let letters = "abcdefg";
+
+for(let i = 0; i < letters.length; i++) {
+    let currentLetter = letters[i];
+    console.log(currentLetter)
+}
