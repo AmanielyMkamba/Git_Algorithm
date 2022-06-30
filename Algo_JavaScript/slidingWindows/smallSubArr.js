ccc234e0ae1b84d0ddb01da70a9ab42a585b1b6c
Amani Mkamba <amani.mkamba@gmail.com>
2022-06-29 22:39:55 -0400

    Update smallSubArr.js

--- Algo_JavaScript/slidingWindows/smallSubArr.js
+++ Algo_JavaScript/slidingWindows/smallSubArr.js
@@ -1,38 +1,22 @@
-function max_sub_array_of_size_k(k, arr) {
-    let maxSum = 0,
+function smallest_subarray_sum(s, arr) {
+    let minLength = Infinity,
         windowSum = 0,
         windowStart = 0;
-    for(window_end = 0; window_end < arr.length; window_end++) {
-        windowSum += arr[window_end]; //add next element
-        //slide window, if we hit size "k"
-        if(window_end - windowStart + 1 === k) {
-            maxSum = Math.max(maxSum, windowSum);
-            windowSum -= arr[windowStart]; //Substract going out element
-            windowStart += 1 //slide window ahead
+
+        for(let windowEnd = 0; windowEnd < arr.length; windowEnd++) {
+            windowSum += arr[windowEnd];
+            while (windowSum >= s) {
+                minLength = Math.min(minLength, windowEnd - windowStart + 1);
+                windowSum -= arr[windowStart];
+                windowSum += 1 //sliding window
+            }
         }
-    }
-    return maxSum;
+        if(minLength === Infinity) {
+            return 0;
+        }
+        return minLength
 }
 
-const result = `Maximum sum of a subarray of size K: ${max_sub_array_of_size_k(3, [2, 1, 5, 1, 3, 2])}`
-console.log(result)
-
-
-
-
-
-
-function max_sub_array_of_size_k(k, arr) {
-    let maxSum = 0,
-        windowSum = 0,
-        windowStart = 0;
-    for(window_end = 0; window_end < arr.length; window_end++) {
-        windowSum += arr[window_end];
-        if(window_end - windowStart + 1 === k) {
-            maxSum = Math.max(windowSum, maxSum);
-            windowSum -= arr[windowStart];
-            windowStart += 1
-        }
-    }
-    return maxSum
-}
+console.log(`Smallest subarray length: ${smallest_subarray_sum(7, [2, 1, 5, 2, 3, 2])}`);
+console.log(`Smallest subarray length: ${smallest_subarray_sum(8, [3, 4, 1, 1, 6])}`);
+console.log(`Smallest subarray length: ${smallest_subarray_sum(7, [2, 1, 5, 2, 3, 2])}`);
