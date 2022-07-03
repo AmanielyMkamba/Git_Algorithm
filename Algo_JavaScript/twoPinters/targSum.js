ecb18b77c7e7f47234dc399c7f244400079a7924
Amani Mkamba <amani.mkamba@gmail.com>
2022-07-03 05:45:52 -0400

    Update targSum.js

--- Algo_JavaScript/twoPinters/targSum.js
+++ Algo_JavaScript/twoPinters/targSum.js
@@ -1,13 +1,14 @@
-function targSum(arr, sumNum) {
+/*
+function targSum(arr, targSum) {
     let pOne = 0,
         pTwo = arr.length - 1,
         pSum = 0;
     while(pOne <= pTwo) {
         pSum = arr[pOne] + arr[pTwo];
-        if(pSum === sumNum) {
+        if(pSum === targSum) {
             return [pOne, pTwo]
         }
-        if(pSum < sumNum) {
+        if(pSum < targSum) {
             pOne ++;
         }
         else {
@@ -19,3 +20,18 @@ function targSum(arr, sumNum) {
 
 console.log(targSum([1,2,3,4,6], 6))
 console.log(targSum([2,5,9,11], 11))
+*/
+
+function pairTargSum(arr, targSum) {
+    const nums = {}; //to store numbers(values) and their indices(keys)
+    for(let i = 0; i < arr.length; i++) {
+        const num = arr[i];
+        if(targSum - arr[i] in nums) {
+            return [nums[targSum - num], i];
+        }
+        nums[arr[i]] = i;
+    }
+    return [i, i];
+}
+
+console.log(pairTargSum([1,2,3,4,6], 6))
