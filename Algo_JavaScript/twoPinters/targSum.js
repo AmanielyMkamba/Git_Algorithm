1b450fd9d3de6ecbf46003d0cb3841a289fcf84f
Amani Mkamba <amani.mkamba@gmail.com>
2022-07-05 22:32:44 -0400

    Update targSum.js

--- Algo_JavaScript/twoPinters/targSum.js
+++ Algo_JavaScript/twoPinters/targSum.js
@@ -1,4 +1,4 @@
-/*
+
 function targSum(arr, targSum) {
     let pOne = 0,
         pTwo = arr.length - 1,
@@ -15,23 +15,26 @@ function targSum(arr, targSum) {
             pTwo --;
         }
     }
-    return [pOne,pTwo]
+    return [-1,-1] //Means doesn't exist
 }
 
 console.log(targSum([1,2,3,4,6], 6))
 console.log(targSum([2,5,9,11], 11))
-*/
 
-function pairTargSum(arr, targSum) {
-    const nums = {}; //to store numbers(values) and their indices(keys)
-    for(let i = 0; i < arr.length; i++) {
-        const num = arr[i];
-        if(targSum - arr[i] in nums) {
-            return [nums[targSum - num], i];
-        }
-        nums[arr[i]] = i;
-    }
-    return [i, i];
-}
+/*
 
-console.log(pairTargSum([1,2,3,4,6], 6))
+
+// function pairTargSum(arr, targSum) {
+//     const nums = {}; //to store numbers(values) and their indices(keys)
+//     for(let i = 0; i < arr.length; i++) {
+//         const num = arr[i];
+//         if(targSum - arr[i] in nums) {
+//             return [nums[targSum - num], i];
+//         }
+//         // console.log(nums[targSum-num])
+//         nums[arr[i]] = i;
+//     }
+//     return [i, i];
+// }
+
+// console.log(pairTargSum([1,2,3,4,6], 6))
