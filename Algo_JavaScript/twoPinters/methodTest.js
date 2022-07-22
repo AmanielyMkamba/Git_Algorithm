57131bbf83d2dd49aba92e8d40ae324ee2373040
Amani Mkamba <amani.mkamba@gmail.com>
2022-07-23 02:46:19 +0300

    Update methodTest.js

--- Algo_JavaScript/twoPinters/methodTest.js
+++ Algo_JavaScript/twoPinters/methodTest.js
@@ -1,7 +1,11 @@
 const months = [1,5,3,4,2];
 console.log(months.sort())
 
-const nums = [4, 2000, 20, 10000];
-nums.sort((a, b) => a - b)
-console.log(nums)
+const numsOne = [4, 2000, 20, 10000];
+numsOne.sort((a, b) => a - b)
+console.log(numsOne)
+
+const numsTwo = [-4, -2000, 20, 10000];
+numsTwo.sort((a, b) => a - b)
+console.log(numsTwo)
 
