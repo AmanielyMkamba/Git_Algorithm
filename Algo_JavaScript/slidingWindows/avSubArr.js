03c38208b829a77b952b346481cfd8f162675a29
Amani Mkamba <amani.mkamba@gmail.com>
2022-07-26 23:44:18 +0300

    Create avSubArr.js

---
+++ Algo_JavaScript/slidingWindows/avSubArr.js
@@ -0,0 +1,21 @@
+//Given an array, find the average of all subarrays of ‘K’ contiguous elements in it.
+//Array: [1, 3, 2, 6, -1, 4, 1, 8, 2], K=5
+//Output: [2.2, 2.8, 2.4, 3.6, 2.8]
+
+function find_averages_of_subarrays(K, arr) {
+    const result = [];
+    for (let i = 0; i < arr.length - K + 1; i++) {
+      // find sum of next 'K' elements
+      let sum = 0.0;
+      for (let j = i; j < i + K; j++) {
+        sum += arr[j];
+      }
+      result.push(sum / K); // calculate average
+    }
+  
+    return result;
+  }
+  
+  
+  const result = find_averages_of_subarrays(5, [1, 3, 2, 6, -1, 4, 1, 8, 2]);
+  console.log(`Averages of subarrays of size K: ${result}`);
