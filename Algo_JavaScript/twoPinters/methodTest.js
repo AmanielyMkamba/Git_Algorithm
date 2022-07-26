50a732926a983a1ba6eb533139981c431a7a44c5
Amani Mkamba <amani.mkamba@gmail.com>
2022-07-27 00:05:23 +0300

    Update methodTest.js

--- Algo_JavaScript/twoPinters/methodTest.js
+++ Algo_JavaScript/twoPinters/methodTest.js
@@ -1,11 +1,14 @@
-const months = [1,5,3,4,2];
-console.log(months.sort())
+// const months = [1,5,3,4,2];
+// console.log(months.sort())
 
-const numsOne = [4, 2000, 20, 10000];
-numsOne.sort((a, b) => a - b)
-console.log(numsOne)
+// const numsOne = [4, 2000, 20, 10000];
+// numsOne.sort((a, b) => a - b)
+// console.log(numsOne)
 
-const numsTwo = [-4, -2000, 20, 10000];
-numsTwo.sort((a, b) => a - b)
-console.log(numsTwo)
+// const numsTwo = [-4, -2000, 20, 10000];
+// numsTwo.sort((a, b) => a - b)
+// console.log(numsTwo)
+
+const person = "Sheena";
+console.log(person.length)
 
