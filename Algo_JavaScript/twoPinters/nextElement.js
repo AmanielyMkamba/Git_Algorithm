104c0f48a6fb616506e46bea9d03df772dd06227
Amani Mkamba <amani.mkamba@gmail.com>
2022-07-27 22:45:21 +0300

    Update nextElement.js

--- Algo_JavaScript/twoPinters/nextElement.js
+++ Algo_JavaScript/twoPinters/nextElement.js
@@ -11,3 +11,14 @@ function removeElement(arr, key) {
 
 //                       i 0  1  2
 console.log(removeElement([3, 2, 3, 6, 3, 10, 9, 3], 3));
+
+function removeElement(arr, key) {
+    let nextElement = 0;
+    for(i = 0; i < arr.length; i++) {
+        if(arr[i] !== key) {
+            arr[nextElement] = arr[i];
+            nextElement += 1;
+        }
+    }
+    return nextElement
+}
