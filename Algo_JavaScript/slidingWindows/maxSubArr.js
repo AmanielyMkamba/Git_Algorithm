f1f889089dbd016165e067cf4ab543e82be12f9c
Amani Mkamba <amani.mkamba@gmail.com>
2022-08-27 19:35:24 -0400

    Update maxSubArr.js

--- Algo_JavaScript/slidingWindows/maxSubArr.js
+++ Algo_JavaScript/slidingWindows/maxSubArr.js
@@ -29,3 +29,24 @@ console.log(maxSubArr([2, 1, 5, 1, 3, 2], k=3))
 // console.log(maxSubArr([2,2,2], k=3))
 
 
+function maxSubArr(arr, k) {
+    let maxSum = 0,
+        windowSum = 0,
+        windowStart = 0;
+
+     for(let windowEnd = 0; windowEnd < arr.length; windowEnd++) {
+         windowSum += arr[windowEnd];
+        if(windowEnd <= k - 1) {
+            maxSum += Math.max(maxSum, windowSum);
+            windowSum -= arr[windowStart];
+            windowStart += 1;
+        }
+    }
+    return maxSum
+}
+
+console.log(maxSubArr([2, 1, 5, 1, 3, 2], k=3))
+// console.log(maxSubArr([2, 1, -5, 1, 3, 2], k=3))
+// console.log(maxSubArr([2,2,2], k=3))
+
+
