acc804a2787441122e8c9e393113998b85875a88
Amani Mkamba <amani.mkamba@gmail.com>
2022-08-29 23:06:40 -0400

    Update smallSubArr.js

--- Algo_JavaScript/slidingWindows/smallSubArr.js
+++ Algo_JavaScript/slidingWindows/smallSubArr.js
@@ -28,3 +28,34 @@ function smallest_subarray_sum(s, arr) {
 console.log(`Smallest subarray length: ${smallest_subarray_sum(7, [2, 1, 5, 2, 3, 2])}`);
 console.log(`Smallest subarray length: ${smallest_subarray_sum(8, [3, 4, 1, 1, 6])}`);
 console.log(`Smallest subarray length: ${smallest_subarray_sum(7, [2, 1, 5, 2, 3, 2])}`);
+
+// //Given an array of positive integers and a number ‘S,’ find the length of the smallest contiguous subarray whose sum is greater than or equal to ‘S’. Return 0 if no such subarray exists.
+// //Input: [2, 1, 5, 2, 3, 2], S=7
+// Output: 2
+// Explanation: The smallest subarray with a sum greater than or equal to ‘7’ is [5, 2].
+// //Input: [2, 1, 5, 2, 8], S=7
+// Output: 1
+// Explanation: The smallest subarray with a sum greater than or equal to ‘7’ is [8].
+
+function smallest_subarray_sum(s, arr) {
+    let minLength = Infinity,
+        windowSum = 0,
+        windowStart = 0;
+
+        for(let windowEnd = 0; windowEnd < arr.length; windowEnd++) {
+            windowSum += arr[windowEnd];
+            while (windowSum >= s) {
+                minLength = Math.min(minLength, windowEnd - windowStart + 1);
+                windowSum -= arr[windowStart];
+                windowStart += 1 //sliding window
+            }
+        }
+        if(minLength === Infinity) {
+            return 0;
+        }
+        return minLength
+}
+
+console.log(`Smallest subarray length: ${smallest_subarray_sum(7, [2, 1, 5, 2, 3, 2])}`);
+console.log(`Smallest subarray length: ${smallest_subarray_sum(8, [3, 4, 1, 1, 6])}`);
+console.log(`Smallest subarray length: ${smallest_subarray_sum(7, [2, 1, 5, 2, 3, 2])}`);
